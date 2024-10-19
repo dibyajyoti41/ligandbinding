7e60b710d0d964a1b1801804963743db39f6c136
Dibyajyoti Mohanta <dibyajyoti@Dibyajyotis-MacBook-Air.local>
2024-10-19 07:26:38 -0400

    +OptMap2

---
+++ +OptMap2/+KymoAlignment2/+NRAlign2/pre_nralign_shift_align.m
@@ -0,0 +1,46 @@
+function [shiftAlignedKymo, shiftingVect] = pre_nralign_shift_align(unalignedKymo, rowSmoothingWindowLen_pixels, blurSigmaWidth_pixels)
+    % PRE_NRALIGN_SHIFT_ALIGN - non-recursively aligns a kymograph,
+    %  using a modified wpalign algorithm.
+    %
+    % Inputs:
+    %	unalignedKymo
+    %	  the unaligned kymograph
+    %
+    % Outputs:
+    %   shiftAlignedKymo
+    %     the shift aligned kymograph on which stretch factors were
+    %     computed to produce the aligned kymograph
+    %   shiftingVect
+    %     the amount each row was shifted
+    %
+    % Authors:
+    %	HN, SQ, AD
+
+
+    % Each row of the kymograph is smoothed out in order to reduce noise
+    % SQ: Is this necessary? are these parameters good choices?
+    if nargin < 2
+        rowSmoothingWindowLen_pixels = 10;
+    end
+    if nargin < 3
+        blurSigmaWidth_pixels = 2;
+    end
+    
+    unalignedKymo(isnan(unalignedKymo)) = 0; % in case any nan's we assume 0 (mean background assume zero)
+    
+    rowBlurredUnalignedKymo = unalignedKymo;
+    if rowSmoothingWindowLen_pixels ~= 1
+        hSize = [1, rowSmoothingWindowLen_pixels];
+        blurringKernel = fspecial('gaussian', hSize, blurSigmaWidth_pixels);
+        rowBlurredUnalignedKymo = conv2(rowBlurredUnalignedKymo, blurringKernel, 'valid');
+    end
+
+    %The kymograph is "prealigned" using shift-based alignment
+    % The maximum shifting of each row
+    maxShiftPerRow = 3;
+    import OptMap2.KymoAlignment2.NRAlign2.get_shift_alignments;
+    shiftingVect = get_shift_alignments(rowBlurredUnalignedKymo, maxShiftPerRow);
+
+    import OptMap2.KymoAlignment2.NRAlign2.shift_rows;
+    shiftAlignedKymo = shift_rows(unalignedKymo, shiftingVect);
+end
